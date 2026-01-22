de8ad987f752cd281152911982c9ab2638fcbcdc
cristian7712 <cristian.diaz8918@gmail.com>
2026-01-21 19:28:17 -0500

    Implementacion de .env

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
-    res.send('Hello world')
+    res.send(`
+        <h1>Curso Express.js V2.0</h1>
+        <p>Esto es una aplicacion node.js con express</p>
+        <p>Corre en el puerto: ${PORT}
+        `)
 });
 
-app.listen(port, ()=>{
+app.listen(PORT, ()=>{
     console.log("Escuchando al server express")
 })
